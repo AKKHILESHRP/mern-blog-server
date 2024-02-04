e15afe3db7c2807be8aaecd8cf96ee89fcd589d6
AKKHILESHRP <akkhileshrp2003@gmail.com>
2024-02-04 17:58:41 +0530

    Blog CRUD completd

--- Routes/Blog.js
+++ Routes/Blog.js
@@ -6,6 +6,22 @@ const router = express.Router();
 
 router.use(checkAuthToken);
 
+async function checkBlogAuth(req, res, next) {
+  try {
+    const blog = await blogModel.findById(req.params.id);
+    if (!blog) {
+      res.status(404).send({ message: "Blog not found" });
+    }
+    if (blog.owner.toString() !== req.userId) {
+      return res.status(404).send({ message: "Permission denied" });
+    }
+    req.blog = blog;
+    next();
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+}
+
 router.post("/", checkAuthToken, async (req, res) => {
   try {
     const { title, description, image, paragraphs, category } = req.body;
@@ -25,6 +41,59 @@ router.post("/", checkAuthToken, async (req, res) => {
     user.blogs.push(blog._id);
     await user.save();
     res.status(201).send({ message: "Blog created successfully", blog });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const blog = await blogModel.findById(req.params.id);
+    if (!blog) {
+      res.status(404).send({ message: "Blog not found" });
+    }
+    res.send(blog);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+router.put("/:id", checkAuthToken, checkBlogAuth, async (req, res) => {
+  try {
+    const { title, description, image, paragraphs, category } = req.body;
+    const updatedBlog = await blogModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        title,
+        description,
+        image,
+        paragraphs,
+        category,
+      },
+      { new: true }
+    );
+    if (!updatedBlog) {
+      return res.status(404).send({ message: "Blog not found" });
+    }
+    res.status(201).send({ message: "Blog updated", updatedBlog });
+  } catch (error) {
+    res.status(404).send({ message: message.error });
+  }
+});
+
+router.delete("/:id", checkAuthToken, checkBlogAuth, async (req, res) => {
+  try {
+    const deletedBlog = await blogModel.findByIdAndDelete(req.params.id);
+    if (!deletedBlog) {
+      return res.status(404).send({ message: "Blog not found" });
+    }
+    const user = await userModel.findById(req.userId);
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+    user.blogs.pull(req.params.id);
+    await user.save();
+    res.send({ message: "Blog deleted successfully", deletedBlog });
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
